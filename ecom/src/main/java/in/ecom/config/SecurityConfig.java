f50335a19cc57497326d9ccc56d3289efc0374ef
karansawant054 <karansawant054@gmail.com>
2026-04-02 00:27:08 +0530

    Add files via upload

---
+++ ecom/src/main/java/in/ecom/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package in.ecom.config;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Autowired
+    JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    	http.csrf(csrf -> csrf.disable())
+    	.sessionManagement(session -> 
+        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    )
+    	.authorizeHttpRequests(auth -> auth
+    	        .requestMatchers("/api/auth/**").permitAll()
+    	        .requestMatchers("/api/products/**").hasRole("ADMIN")
+    	        .requestMatchers("/api/orders/**").hasRole("USER")
+    	        .anyRequest().authenticated()
+    	    );
+            
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+        
+    
+    }
+
+}
