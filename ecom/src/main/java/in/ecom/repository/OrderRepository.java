8c343db3bb40b6cb4ae07e2e303f3dd2c0caba72
karansawant054 <karansawant054@gmail.com>
2026-04-01 22:49:25 +0530

    Delete ecom directory

--- ecom/src/main/java/in/ecom/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package in.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.ecom.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
