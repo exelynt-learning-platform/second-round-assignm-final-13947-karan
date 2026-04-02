61389dcee9785ab1b0498c4d6fef53c2ab1f9fdb
karansawant054 <karansawant054@gmail.com>
2026-04-02 13:29:22 +0530

    Delete ecom directory

--- ecom/src/main/java/in/ecom/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package in.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import in.ecom.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
